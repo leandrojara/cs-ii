f61f501d00587d155000bae38b9aad5ad8d9cc4e
LeandroJara <leandro.souza.jara@hotmail.com>
2018-08-20 21:32:08 -0400

    imovel

--- application/home/home/src/main/java/br/ufms/facom/home/controllers/ImovelController.java
+++ application/home/home/src/main/java/br/ufms/facom/home/controllers/ImovelController.java
@@ -4,35 +4,31 @@ import br.ufms.facom.home.domain.Anunciante;
 import br.ufms.facom.home.domain.Imovel;
 import br.ufms.facom.home.repository.ImovelRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-import java.util.Arrays;
-import java.util.List;
+import javax.validation.Valid;
+import java.util.Date;
 
-@RestController
-@RequestMapping("/imovel")
+@Controller
 public class ImovelController {
 
     @Autowired
     private ImovelRepository imovelRepository;
 
-    @PostMapping(value = "/salvar/{anuncianteId}")
-    public Imovel salvar(@PathVariable Long anuncianteId, @RequestBody Imovel imovel) {
-        imovel.setAnunciante(new Anunciante());
-        imovel.getAnunciante().setId(anuncianteId);
-        imovelRepository.save(imovel);
-        return imovel;
+    @RequestMapping(value = "/imovel/anunciar", method = RequestMethod.GET)
+    public String anunciar(Model model) {
+        model.addAttribute("imovel", new Imovel());
+        return "imovel/anunciar";
     }
 
-    @GetMapping(value = "buscar/{anuncianteId}/{imovelId}")
-    public List<Imovel> buscar(@PathVariable(required = false) Long anuncianteId,
-                               @PathVariable(required = false) Long imovelId) {
-        if (anuncianteId != null && anuncianteId > 0) {
-            return imovelRepository.findByAnuncianteId(anuncianteId);
-        } else if (imovelId != null && imovelId > 0) {
-            return Arrays.asList(imovelRepository.findById(imovelId).get());
-        } else {
-            return imovelRepository.findAll();
-        }
+    @RequestMapping(value = "/imovel/salvar", method = RequestMethod.POST)
+    public String salvar(@Valid Imovel imovel, Model model) {
+        imovel.setDataCadastro(new Date());
+        imovel.setAnunciante((Anunciante) model.asMap().get("anunciante"));
+        imovelRepository.save(imovel);
+        return anunciar(model);
     }
 }
