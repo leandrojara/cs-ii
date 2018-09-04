4be38524fe6f29f3e3b2fbfdc0dced52be3cf453
paulorizardi <p.rsrizardi@gmail.com>
2018-09-03 20:30:22 -0400

    data.sql

--- application/home/home/src/main/resources/data.sql
+++ application/home/home/src/main/resources/data.sql
@@ -3,3 +3,5 @@ insert into adicional_imovel(id, descricao) select 2, 'PLAYGROUD' where not exis
 insert into adicional_imovel(id, descricao) select 3, 'DESPENSA' where not exists (select id from adicional_imovel where id = 3);
 insert into adicional_imovel(id, descricao) select 4, 'SALÃO DE FESTAS' where not exists (select id from adicional_imovel where id = 4);
 insert into adicional_imovel(id, descricao) select 5, 'ÁREA DE SERVIÇO' where not exists (select id from adicional_imovel where id = 5);
+
+insert into usuario(id, nome, telefone, email, senha, DTYPE) values (1, 'user', '(67) 99999-9999', 'email@email.com', '123', 'Anunciante');
