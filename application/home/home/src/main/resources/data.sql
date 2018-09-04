9731451ce43cf7fbe0f184895332545b3b21c808
leandrojara <leandro.souza.jara@hotmail.com>
2018-09-03 20:34:05 -0400

    data.sql

--- application/home/home/src/main/resources/data.sql
+++ application/home/home/src/main/resources/data.sql
@@ -4,4 +4,4 @@ insert into adicional_imovel(id, descricao) select 3, 'DESPENSA' where not exist
 insert into adicional_imovel(id, descricao) select 4, 'SALÃO DE FESTAS' where not exists (select id from adicional_imovel where id = 4);
 insert into adicional_imovel(id, descricao) select 5, 'ÁREA DE SERVIÇO' where not exists (select id from adicional_imovel where id = 5);
 
-insert into usuario(id, nome, telefone, email, senha, DTYPE) values (1, 'user', '(67) 99999-9999', 'email@email.com', '123', 'Anunciante');
+insert into usuario(id, nome, telefone, email, senha, DTYPE) values (1, 'user', '(67) 99999-9999', 'email@email.com', '$2a$07$fi5QDS4w9KcGoLPhjraowejID5dihUCsSrz62Ns3/Rx5JR/5FNI0C', 'Anunciante');
