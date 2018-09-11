8601150efed4ffdb68a0dcf1731a6ea2c6de3aa6
leandrojara <leandro.souza.jara@hotmail.com>
2018-09-10 21:15:35 -0400

    test ok

--- application/home/home/src/test/java/br/ufms/facom/home/repository/AdicionalImovelRepositoryTest.java
+++ application/home/home/src/test/java/br/ufms/facom/home/repository/AdicionalImovelRepositoryTest.java
@@ -1,4 +1,19 @@
 package br.ufms.facom.home.repository;
 
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class AdicionalImovelRepositoryTest {
+
+    @Autowired
+    private AdicionalImovelRepository adicionalImovelRepository;
+
+    @Test
+    public void verificaBancoPovoadoAutomaticamente() {
+        Assertions.assertThat(adicionalImovelRepository.findAll()).isNotNull();
+        Assertions.assertThat(adicionalImovelRepository.findAll()).isNotEmpty();
+        Assertions.assertThat(adicionalImovelRepository.findById(1L).get()).isNotNull();
+        Assertions.assertThat(adicionalImovelRepository.findById(1L).get().getDescricao()).isEqualTo("CHURRASQUEIRA");
+    }
 }
