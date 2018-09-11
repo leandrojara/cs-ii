5d38c93df34aa50023ce28a72a92cb3aa3ec4a13
leandrojara <leandro.souza.jara@hotmail.com>
2018-09-10 21:46:21 -0400

    tests ok

--- application/home/home/src/test/java/br/ufms/facom/home/services/ImovelServicesTest.java
+++ application/home/home/src/test/java/br/ufms/facom/home/services/ImovelServicesTest.java
@@ -1,4 +1,28 @@
 package br.ufms.facom.home.services;
 
+import br.ufms.facom.home.domain.Imovel;
+import br.ufms.facom.home.repository.AdicionalImovelRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class ImovelServicesTest {
+
+    @Autowired
+    private ImovelServices imovelServices;
+    @Autowired
+    private AdicionalImovelRepository adicionalImovelRepository;
+
+    @Test
+    public void addAdicionaisTest() {
+        Imovel imovel = new Imovel();
+        Assertions.assertThat(imovel.getAdicionais()).isNull();
+
+        imovelServices.addAdicionais(imovel, new long[]{1, 2, 3});
+        Assertions.assertThat(imovel.getAdicionais()).isNotNull();
+        Assertions.assertThat(imovel.getAdicionais()).isNotEmpty();
+        Assertions.assertThat(imovel.getAdicionais()).hasSize(3);
+        Assertions.assertThat(imovel.getAdicionais().contains(adicionalImovelRepository.getOne(1L))).isEqualTo(true);
+        Assertions.assertThat(imovel.getAdicionais().contains(adicionalImovelRepository.getOne(4L))).isEqualTo(false);
+    }
 }
