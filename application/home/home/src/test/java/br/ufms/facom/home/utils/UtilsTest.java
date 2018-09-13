e539ac9e403cffcd9210d7873fca220b84f929dd
leandrojara <leandro.souza.jara@hotmail.com>
2018-09-12 22:11:01 -0400

    test ok

--- application/home/home/src/test/java/br/ufms/facom/home/utils/UtilsTest.java
+++ application/home/home/src/test/java/br/ufms/facom/home/utils/UtilsTest.java
@@ -10,7 +10,5 @@ public class UtilsTest {
         String encoded = Utils.getEncoder().encode("123");
         Assertions.assertThat(Utils.getEncoder().matches("123", encoded)).isEqualTo(true);
         Assertions.assertThat(Utils.getEncoder().matches("1234", encoded)).isEqualTo(false);
-        Assertions.assertThat(Utils.getEncoder().encode("123").equals(Utils.encrypt("123"))).isEqualTo(true);
-        Assertions.assertThat(Utils.getEncoder().encode("123").equals(Utils.encrypt("1234"))).isEqualTo(false);
     }
 }
