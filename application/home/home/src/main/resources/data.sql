7e5e8b443ddb563b15cfecfff673bdac17fbcc07
LeandroJara <leandro.souza.jara@hotmail.com>
2018-09-24 19:59:57 -0400

    pagination

--- application/home/home/src/main/resources/data.sql
+++ application/home/home/src/main/resources/data.sql
@@ -4,4 +4,12 @@ insert into adicional_imovel(id, descricao) select 3, 'DESPENSA' where not exist
 insert into adicional_imovel(id, descricao) select 4, 'SALÃO DE FESTAS' where not exists (select id from adicional_imovel where id = 4);
 insert into adicional_imovel(id, descricao) select 5, 'ÁREA DE SERVIÇO' where not exists (select id from adicional_imovel where id = 5);
 
--- insert into usuario(id, nome, telefone, email, senha, DTYPE, bloqueado) values (1, 'user', '(67) 99999-9999', 'email@email.com', '$2a$07$fi5QDS4w9KcGoLPhjraowejID5dihUCsSrz62Ns3/Rx5JR/5FNI0C', 'Anunciante', false);
+insert into usuario(id, nome, telefone, email, senha, DTYPE, bloqueado) values (1, 'user', '(67) 99999-9999', 'email@email.com', '$2a$07$fi5QDS4w9KcGoLPhjraowejID5dihUCsSrz62Ns3/Rx5JR/5FNI0C', 'Anunciante', false);
+
+insert into imovel(id, rua, numero, bairro, cidade, estado, id_anunciante, tipo_imovel, valor_imovel) values (1, 'Dr Lelis Espartel 1', 1, 'Tijuca', 'Campo Grande', 'MS', 1, 'CASA', 0);
+insert into imovel(id, rua, numero, bairro, cidade, estado, id_anunciante, tipo_imovel, valor_imovel) values (2, 'Dr Lelis Espartel 2', 2, 'Tijuca', 'Campo Grande', 'MS', 1, 'CASA', 0);
+insert into imovel(id, rua, numero, bairro, cidade, estado, id_anunciante, tipo_imovel, valor_imovel) values (3, 'Dr Lelis Espartel 3', 3, 'Tijuca', 'Campo Grande', 'MS', 1, 'CASA', 0);
+insert into imovel(id, rua, numero, bairro, cidade, estado, id_anunciante, tipo_imovel, valor_imovel) values (4, 'Dr Lelis Espartel 4', 4, 'Tijuca', 'Campo Grande', 'MS', 1, 'CASA', 0);
+insert into imovel(id, rua, numero, bairro, cidade, estado, id_anunciante, tipo_imovel, valor_imovel) values (5, 'Dr Lelis Espartel 5', 5, 'Tijuca', 'Campo Grande', 'MS', 1, 'CASA', 0);
+insert into imovel(id, rua, numero, bairro, cidade, estado, id_anunciante, tipo_imovel, valor_imovel) values (6, 'Dr Lelis Espartel 6', 6, 'Tijuca', 'Campo Grande', 'MS', 1, 'CASA', 0);
+insert into imovel(id, rua, numero, bairro, cidade, estado, id_anunciante, tipo_imovel, valor_imovel) values (7, 'Dr Lelis Espartel 7', 7, 'Tijuca', 'Campo Grande', 'MS', 1, 'CASA', 0);
